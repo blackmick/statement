984d22940a470321ef8925aa927700bd380f2528
Shawn Hans <shawn@ShawndeiMac.local>
2015-12-17 10:42:14 +0800

    working copy

---
+++ agent/src/main/java/com/shawn/finance/statement/Agent.java
@@ -0,0 +1,13 @@
+package com.shawn.finance.statement;
+
+/**
+ * Hello world!
+ *
+ */
+public class Agent
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
